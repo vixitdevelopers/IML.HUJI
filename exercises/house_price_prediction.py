fea343c696d6137cb7956eeaafaaf06a4512836b
vixitdevelopers <69685413+vixitdevelopers@users.noreply.github.com>
2023-04-23 13:54:03 +0300

    doesn't work but worked hard

--- exercises/house_price_prediction.py
+++ exercises/house_price_prediction.py
@@ -10,6 +10,10 @@ import plotly.io as pio
 
 pio.templates.default = "simple_white"
 
+averages = {}
+train_columns = None
+train_dummies = None
+
 
 def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
     """
@@ -27,32 +31,76 @@ def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
     Post-processed design matrix and response vector (prices) - either as a single
     DataFrame or a Tuple[DataFrame, Series]
     """
+    # If it is the training set, remove all irrelevant rows (that are missing values):
+    global averages, train_columns, train_dummies
+
+    X_proc = pd.DataFrame.copy(X)
+
+    if y is None:
+        for col in X_proc.columns:
+            X_proc[col] = X_proc[col].apply(replace_nonsensical_values, args=(col,))
+        X_proc['date'] = pd.to_numeric(X_proc['date'].str[:8])
+    else:
+        y_proc = pd.DataFrame.copy(y)
+        X_proc = X_proc.dropna(axis=0)
+        # identify rows with NaN values in X
+        nan_rows = X.isna().any(axis=1)
+        # remove rows with NaN values in X and corresponding rows in y
+        X_proc = X_proc[~nan_rows]
+        y_proc = y_proc[~nan_rows]
+
+        X_proc['date'] = pd.to_numeric(X_proc['date'].str[:8])
+        mask = pd.Series(True, index=X_proc.index)
+        mask &= X_proc.notna().all(axis=1)
+        for col in X_proc.columns:
+            mask &= sensical_values_indices(X_proc, col)
+        X_proc = X_proc[mask]
+        y_proc = y_proc[mask]
+        for col in X_proc.columns:
+            averages[col] = X_proc[col].mean()
+
     # add age and age_renovated (which will just be the age if never renovated)
-    X['age'] = X.apply(lambda x: int(x.date[0:4]) - x.yr_built, axis=1)
-    X['age_renovated'] = X.apply(lambda x:
-                                 int(x.date[0:4]) - x.yr_renovated if x.yr_renovated > 0 else x.age, axis=1)
-    # convert date to pandas date_time
-    X['date'] = pd.to_datetime(X['date'], format='%Y%m%dT%H%M%S').dt.date
+    X_proc['age'] = X_proc.apply(lambda x: int(x.date[0:4]) - x.yr_built, axis=1)
+    X_proc['age_renovated'] = X_proc.apply(lambda x:
+                                           int(x.date[0:4]) - x.yr_renovated if x.yr_renovated > 0 else x.age, axis=1)
 
-    # keep only first 4 digits of zipcode
-    X['zipcode'] = X['zipcode'].str[:4]
+    dummies = pd.get_dummies(X_proc['zipcode'], prefix='zipcode_')
+    dummies['zipcode_other'] = 0
+    X_proc = X_proc.drop('zipcode', axis=1)
+    X_proc = X_proc.drop('id', axis=1)
+    if y is None:
+        missing_cols = set(train_dummies) - set(dummies)
+        for col in missing_cols:
+            dummies[col] = 0
+        other_cols = set(dummies) - set(train_dummies)
+        if other_cols:
+            dummies['zipcode_other'] = (dummies[other_cols].isin([1]).any(axis=1)).astype(int)
 
-    for col in X.columns:
-        X[col] = X[col].apply(replace_nonsensical_values, args=(col,))
+    X_proc = pd.concat([X_proc, dummies], axis=1)
 
-    # create dummy variables from the 'zipcode' column
-
-
-#     todo create dummies
+    if y is not None:
+        train_dummies = dummies
+        train_columns = X_proc.columns
+        return X_proc, y_proc
+    return X[train_columns]
 
 
 def replace_nonsensical_values(x, col_name):
+    global averages
+    if x is None:
+        return averages.get(col_name) if averages.get(col_name) else 0
     if col_name == 'long':
         if x > 0:
-            return -122  # todo save averages
+            return averages.get(col_name) if averages.get(col_name) else 0
     else:
         if x < 0:
-            return 0  # todo save averages
+            return averages[col_name] if averages.get(col_name) else 0
+
+
+def sensical_values_indices(X: pd.DataFrame, col_name: str):
+    if col_name == 'long':
+        return X[col_name] < 0
+    return X[col_name] > 0
 
 
 def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") -> NoReturn:
@@ -72,26 +120,39 @@ def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") ->
     output_path: str (default ".")
         Path to folder in which plots are saved
     """
-
-    A = X["id"]
-    pearson_A = y.cov(A) / (y.std() * A.std())
-
-    # pearson_correlation = lambda a, b: a.cov(b) / (a.std() * b.std())
+    cols = []
+    coals = []
+    for col in X.columns:
+        cols.append(col)
+        A = X[col].astype(float)
+        pearson_A = y.cov(A) / (y.std() * A.std())
+        coals.append(round(pearson_A, 2))
+        both = pd.concat([A, y], axis=1)
+        fig = px.scatter(both, x=col, y='price',
+                         title=f'Correlation between {col} and response'
+                               f'\nPearson correlation: {pearson_A}')
+        # fig.write_image(f'{output_path}_{col}.png')
+    print(cols, '\n', coals)
 
 
 if __name__ == '__main__':
     np.random.seed(0)
     df = pd.read_csv("../datasets/house_prices.csv")
+    # any prices that are Nan are irrelevant (both train and test):
+    df.dropna(subset=['price'], inplace=True)
+    # split to X and y:
     df_response = df['price']
     df = df.drop('price', axis=1)
+
     # Question 1 - split data into train and test sets
     train_x, train_y, test_x, test_y = split_train_test(df, df_response)
 
     # Question 2 - Preprocessing of housing prices dataset
-    # raise NotImplementedError()
+    train_x, train_y = preprocess_data(train_x, train_y)
+    print(train_x)
 
     # Question 3 - Feature evaluation with respect to response
-    # raise NotImplementedError()
+    feature_evaluation(train_x, train_y, output_path='../correlations/')
 
     # Question 4 - Fit model over increasing percentages of the overall training data
     # For every percentage p in 10%, 11%, ..., 100%, repeat the following 10 times:
@@ -103,7 +164,7 @@ if __name__ == '__main__':
     for percent in range(10, 101):
         for _ in range(10):
             X, _, y, _ = split_train_test(train_x, train_y, (percent / 100))  # todo: randomly
-            linear_model = LinearRegression()
+            linear_model = LinearRegression(include_intercept=True)
             linear_model.fit(X, y)
             loss = linear_model.loss(test_x, test_y)
             loss_mean, loss_std = pd.Series(loss).mean(), pd.Series(loss).std()
